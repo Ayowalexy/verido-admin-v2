9aac0abe7513744e8ba7f763003dc186fef3c881
Ayowalexy <seinde4@gmail.com>
2022-07-16 18:38:20 +0100

    initial push

--- public/services/routes.tsx
+++ public/services/routes.tsx
@@ -1,11 +1,14 @@
 const baseUrl = 'http://localhost:5000'
+const liveUrl = 'https://verido-v6.herokuapp.com'
+
+const url = liveUrl
 
 const urls = {
     AUTH: {
-        register: `${baseUrl}/user/register`,
-        login: `${baseUrl}/user/login`,
-        reset: `${baseUrl}/user/reset`,
-        socials: `${baseUrl}/user/socials`
+        register: `${url}/user/register`,
+        login: `${url}/user/login`,
+        reset: `${url}/user/reset`,
+        socials: `${url}/user/socials`
     }
 }
 
